3ba1d193c350b1e5b4d8ac31b07eabc2c3bbe3ac
Carlos Moratelli <crmoratelli@gmail.com>
2017-05-10 20:16:13 -0300

    Initial commit.

---
+++ arch/mips/include/asm/mach-baikal/gpio.h
@@ -0,0 +1,28 @@
+/*
+ * Baikal-T SOC platform support code.
+ *
+ * Copyright (C) 2014  Baikal Electronics OJSC
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+#ifndef __ASM_ARM_ARCH_GPIO_H
+#define __ASM_ARM_ARCH_GPIO_H
+
+#define ARCH_NR_GPIOS 40
+
+#include_next <gpio.h>
+
+#endif /* __ASM_ARM_ARCH_GPIO_H */
+
